d69d5e2e3b378265b43193aec9971c876ea0d883
Yuki Shimada <yukiema@gmail.com>
2015-03-11 21:00:12 +0900

    MapTexturesとMapTileTypesのフィクスチャを追加。

--- 01_Dev/server/fixtures.js
+++ 01_Dev/server/fixtures.js
@@ -16,3 +16,56 @@ if (Maps.find().count() === 0) {
   });
 }
 
+
+if (MapTextures.find().count() === 0) {
+  MapTextures.insert({
+    name: 'メタル',
+    gametex_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/tileImage/gameTile/Metal_lightgray.png',
+    tooltex_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/tileImage/toolTile/Metal_lightgray.jpg'
+  });
+  MapTextures.insert({
+    name: '水晶',
+    gametex_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/tileImage/gameTile/suisyou1.png',
+    tooltex_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/tileImage/toolTile/suisyou1.jpg'
+  });
+  MapTextures.insert({
+    name: '青いタイル',
+    gametex_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/tileImage/gameTile/bluetile.png',
+    tooltex_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/tileImage/toolTile/bluetile.jpg'
+  });
+  MapTextures.insert({
+    name: '虎の毛皮',
+    gametex_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/tileImage/gameTile/FauxFur.png',
+    tooltex_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/tileImage/toolTile/FauxFur.jpg'
+  });
+  MapTextures.insert({
+    name: '木目',
+    gametex_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/tileImage/gameTile/wood.png',
+    tooltex_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/tileImage/toolTile/wood.jpg'
+  });
+}
+
+
+if (MapTileTypes.find().count() === 0) {
+  MapTileTypes.insert({
+    name: '通常',
+    identifier: 'N',
+    image_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/typeTypeImage/normal.png'
+  });
+  MapTileTypes.insert({
+    name: '壁',
+    identifier: 'W',
+    image_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/typeTypeImage/Wall.png'
+  });
+  MapTileTypes.insert({
+    name: 'ダークゾーン',
+    identifier: 'Dk',
+    image_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/typeTypeImage/darkzone.png'
+  });
+  MapTileTypes.insert({
+    name: 'プラットフォーム',
+    identifier: 'P',
+    image_url: 'http://www.emastation.net/uploadspace/WebRPGTool/material/typeTypeImage/platform.png'
+  });
+}
+

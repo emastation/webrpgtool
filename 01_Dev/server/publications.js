6e6f21abfbef0518141136dc9e536af6b607d4da
Yuki Shimada <yukiema@gmail.com>
2015-03-12 08:02:25 +0900

    MapTexturesとMapTileTypesのパブリケーションを追加。

--- 01_Dev/server/publications.js
+++ 01_Dev/server/publications.js
@@ -2,3 +2,10 @@ Meteor.publish('maps', function() {
   return Maps.find();
 });
 
+Meteor.publish('map_textures', function() {
+  return MapTextures.find();
+});
+
+Meteor.publish('map_tile_types', function() {
+  return MapTileTypes.find();
+});

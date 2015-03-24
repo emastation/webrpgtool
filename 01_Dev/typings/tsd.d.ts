3a147ee0ce1df2fdf113980f9a28aa925762168b
Yuki Shimada <yukiema@gmail.com>
2015-03-24 22:34:25 +0900

    meteor及びtypescript系のパッケージのバージョンを最新にした。typingsフォルダ以下のd.tsファイルを削除（meteortypescript:typescript-libsを導入済みのため）

--- 01_Dev/typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="meteor/meteor.d.ts" />
-/// <reference path="underscore/underscore.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="bootstrap.datepicker/bootstrap.datepicker.d.ts" />
-/// <reference path="bootstrap/bootstrap.d.ts" />

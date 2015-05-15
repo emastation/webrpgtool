c1eb83cfe6eab8ad971350eb867d6f6a4c22cac6
Yuki Shimada <yukiema@gmail.com>
2015-05-15 18:02:22 +0900

    フライモードをオフにした。

--- 01_Dev/client/templates/game/_lib/game_state.ts
+++ 01_Dev/client/templates/game/_lib/game_state.ts
@@ -34,7 +34,7 @@ module WrtGame {
   export var L_EAST = "L_EAST";
   export var L_SOUTH = "L_SOUTH";
 
-  export var flyMode_f = true;
+  export var flyMode_f = false;
 
   /**
    *  ゲームの状態を保持するクラス

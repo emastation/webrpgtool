70cb269a7e83710f2be945f81ac1972a3ba59c16
Yuki Shimada <yukiema@gmail.com>
2015-06-04 20:08:16 +0900

    ReactでStoryの新規登録ができるようになった。

--- 01_Dev/client/templates/stories/stories.jsx
+++ 01_Dev/client/templates/stories/stories.jsx
@@ -5,6 +5,12 @@ var StoryList = ReactMeteor.createClass({
     Meteor.subscribe("stories");
   },
 
+  getInitialState: function() {
+    return {
+      newStoryTitle: ''
+    };
+  },
+
   getMeteorState: function() {
     var stories = Stories.find().fetch();
     return {
@@ -18,6 +24,33 @@ var StoryList = ReactMeteor.createClass({
     return <Story key={_id} story={model} />;
   },
 
+  newStoryTitleChange: function(e) {
+    this.setState({
+      newStoryTitle: e.target.value
+    });
+  },
+
+  submitNewStory: function(e) {
+    e.preventDefault();
+
+    var story = {
+      title: this.state.newStoryTitle.trim()
+    };
+
+    Meteor.call('storyInsert', story, function(error, result) { // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+
+      if (result.storyExists)
+        return alert('This title has already been posted');
+
+    });
+
+    this.setState({
+      newStoryTitle: ''
+    });
+  },
+
   render: function() {
     var stories = [
         <div className="StoryList" key="1">
@@ -25,7 +58,20 @@ var StoryList = ReactMeteor.createClass({
         </div>
     ];
 
-    return <div className="inner">{ stories }</div>;
+    var form = <form className="main form" onSubmit={this.submitNewStory}>
+      <div className="form-group">
+        <label className="control-label" htmlFor="title">タイトル</label>
+        <div className="controls">
+          <input name="title" id="title" type="text" value={this.state.newStoryTitle} placeholder="Name your new story's title." className="form-control" onChange={this.newStoryTitleChange}/>
+        </div>
+      </div>
+      <input type="submit" value="Submit" className="btn btn-primary"/>
+    </form>;
+
+    return <div className="StoryList">
+        { form }
+        <div className="inner">{ stories }</div>
+      </div>;
   }
 
 });

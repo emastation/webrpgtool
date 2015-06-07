dd4bc6405d742bb80e2fe8c3110a0ba7fa909899
Yuki Shimada <yukiema@gmail.com>
2015-06-07 14:33:35 +0900

    Storiesリストにおいて、Storyの追加が、きちんとorder情報も付加する形でできるようになった。storyInsertメソッドの場所をサーバー側に移した。

--- 01_Dev/lib/collections/story/stories.js
+++ 01_Dev/lib/collections/story/stories.js
@@ -12,33 +12,3 @@ Stories.deny({
   }
 });
 */
-
-Meteor.methods({
-  storyInsert: function(storyAttributes) {
-    check(Meteor.userId(), String);
-    check(storyAttributes, {
-      title: String
-    });
-
-    var storyWithSameTitle = Stories.findOne({title: storyAttributes.title});
-    if (storyWithSameTitle) {
-      return {
-        storyExists: true,
-        _id: storyWithSameTitle._id
-      }
-    }
-
-    var user = Meteor.user();
-    var story = _.extend(storyAttributes, {
-      userId: user._id,
-      author: user.username,
-      submitted: new Date()
-    });
-
-    var storyId = Stories.insert(story);
-
-    return {
-      _id: storyId
-    };
-  }
-});
--- 01_Dev/server/lib/collections/stories.js
+++ 01_Dev/server/lib/collections/stories.js
@@ -1,1 +1,35 @@
 Sortable.collections = ['stories'];
+
+
+Meteor.methods({
+  storyInsert: function(storyAttributes) {
+    check(Meteor.userId(), String);
+    check(storyAttributes, {
+      title: String
+    });
+
+    var storyWithSameTitle = Stories.findOne({title: storyAttributes.title});
+    if (storyWithSameTitle) {
+      return {
+        storyExists: true,
+        _id: storyWithSameTitle._id
+      }
+    }
+
+    var count = Stories.find().count();
+
+    var user = Meteor.user();
+    var story = _.extend(storyAttributes, {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date(),
+      order: count
+    });
+
+    var storyId = Stories.insert(story);
+
+    return {
+      _id: storyId
+    };
+  }
+});

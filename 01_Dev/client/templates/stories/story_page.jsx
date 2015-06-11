5f56a015033d5d17ed9baecfe191ee386a11870e
Yuki Shimada <yukiema@gmail.com>
2015-06-12 08:20:22 +0900

    Storyページで、Sentenceの編集ができるようになった。

--- 01_Dev/client/templates/stories/story_page.jsx
+++ 01_Dev/client/templates/stories/story_page.jsx
@@ -1,5 +1,17 @@
 var Sentence = React.createClass({
 
+  getInitialState: function() {
+    return {
+      editable: false
+    };
+  },
+
+  editableThisStory: function() {
+    this.setState({
+      editable: true
+    });
+  },
+
   insertSentence: function(id) {
     var storyItemModelClicked = StoryItems.findOne(id);
     var attributes = {
@@ -13,6 +25,33 @@ var Sentence = React.createClass({
       if (error)
         return alert(error.reason);
 
+    });
+  },
+
+  completeEditing: function(id, evt) {
+
+    if (!this.props.meteorUserExist) {
+      return;
+    }
+    if (!_.isUndefined(evt.keyCode) && evt.keyCode !== 13) {// 何らかのキーが押されていて、それがEnterキー以外だった場合
+      return; // 処理を抜ける
+    }
+
+    var sentence = {
+      text: evt.target.textContent
+    };
+
+    Sentences.update(id, {$set: sentence}, function(error) {
+      if (error) {
+        // display the error to the user
+        alert(error.reason);
+      }
+    });
+
+    evt.target.blur();
+
+    this.setState({
+      editable: false
     });
   },
 
@@ -41,11 +80,17 @@ var Sentence = React.createClass({
     }
 
     var text = _.isUndefined(this.props.sentence) ? '' : this.props.sentence.text;
+    var sentenceId = _.isUndefined(this.props.sentence) ? '' : this.props.sentence._id;
+    var contentEditable = this.state.editable && this.props.meteorUserExist;
 
     return <li data-id={this.props.storyItem._id} data-order={this.props.storyItem.order} className="sortable-item removable well well-sm">
       { sortableHandle }
       { plusButton }
-      <span className="name">{text}</span>
+      <span className="name" contentEditable={contentEditable}
+            onClick={this.editableThisStory}
+            onBlur={this.completeEditing.bind(this, sentenceId)}
+            onKeyDown={this.completeEditing.bind(this, sentenceId)}
+          >{text}</span>
       <span className="badge">{this.props.storyItem.order}</span>
       { closeButton }
     </li>

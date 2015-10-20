ef82c4283da6d33c705b194e6c614b2d66f7babe
Yuki Shimada <yukiema@gmail.com>
2015-10-21 07:58:37 +0900

    added collections 'objects' and 'objectSchemata'.

---
+++ 01_Dev/lib/collections/common/object_schemata.js
@@ -0,0 +1,8 @@
+var _global = (this || 0).self || global;
+_global.MongoCollections = _global.MongoCollections || {};
+_global.MongoCollections.ObjectSchemata = new Mongo.Collection('objectSchemata');
+
+_global.MongoCollections.ObjectSchemata.allow({
+  update: function(userId, post) { return ownsDocument(userId, post); },
+  remove: function(userId, post) { return ownsDocument(userId, post); }
+});

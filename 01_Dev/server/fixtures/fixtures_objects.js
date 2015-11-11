b377049675ce8a5f606e8e5a71ab134d130a56c8
Yuki Shimada <yukiema@gmail.com>
2015-11-11 20:54:50 +0900

    add fixture data of 'wrt_game_character' objectSchema.

--- 01_Dev/server/fixtures/fixtures_objects.js
+++ 01_Dev/server/fixtures/fixtures_objects.js
@@ -42,9 +42,128 @@ if (ObjectSchemata.find().count() === 0) {
             name: "敵"
           }
         ]
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        name: '戦闘時の敵としての画像URL',
+        type: 'string',
+        options: []
       }
     ]
   });
+}
+
+var Objects = MongoCollections.Objects;
+if (Objects.find().count() === 0) {
+
+  Objects.insert({
+    identifier: 'Mika@Rabbit',
+    schema_identifier: 'wrt_game_character',
+    attributes: [
+      {
+        identifier: 'name',
+        value: 'ウサギのミカ'
+      },
+      {
+        identifier: 'hp',
+        value: 100
+      },
+      {
+        identifier: 'mp',
+        value: 100
+      },
+      {
+        identifier: 'situation',
+        value: 'friend'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: ''
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'Ayumi@Tartle',
+    schema_identifier: 'wrt_game_character',
+    attributes: [
+      {
+        identifier: 'name',
+        value: 'カメのアユミ'
+      },
+      {
+        identifier: 'hp',
+        value: 80
+      },
+      {
+        identifier: 'mp',
+        value: 120
+      },
+      {
+        identifier: 'situation',
+        value: 'friend'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: ''
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'ork',
+    schema_identifier: 'wrt_game_character',
+    attributes: [
+      {
+        identifier: 'name',
+        value: 'オーク'
+      },
+      {
+        identifier: 'hp',
+        value: 110
+      },
+      {
+        identifier: 'mp',
+        value: 90
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/dh09_po2.png'
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'greenDragon',
+    schema_identifier: 'wrt_game_character',
+    attributes: [
+      {
+        identifier: 'name',
+        value: 'グリーンドラゴン'
+      },
+      {
+        identifier: 'hp',
+        value: 130
+      },
+      {
+        identifier: 'mp',
+        value: 40
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/dh12_po2.png'
+      }
+    ]
+  });
+
 }
 
 wrt_fixtureLoadedCount = (typeof wrt_fixtureLoadedCount !== "undefined") ? wrt_fixtureLoadedCount++ : 0;

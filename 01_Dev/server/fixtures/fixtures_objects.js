242e3d7ca2a44491eb03e67ed0aadbc84da8e1bb
Yuki Shimada <yukiema@gmail.com>
2016-03-24 09:46:39 +0900

    Add Fixture Data

--- 01_Dev/server/fixtures/fixtures_objects.js
+++ 01_Dev/server/fixtures/fixtures_objects.js
@@ -136,7 +136,7 @@ if (Objects.find().count() === 0) {
       },
       {
         identifier: 'battleEnemyImageUrl',
-        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/dh09_po2.png'
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/dh09.png'
       }
     ]
   });
@@ -164,11 +164,318 @@ if (Objects.find().count() === 0) {
       },
       {
         identifier: 'battleEnemyImageUrl',
-        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/dh12_po2.png'
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/dh12.png'
       }
     ]
   });
 
+  Objects.insert({
+    identifier: 'darkNinja',
+    schema_identifier: 'wrt_game_character',
+    game_id: '',
+    attributes: [
+      {
+        identifier: 'name',
+        value: '暗黒忍者'
+      },
+      {
+        identifier: 'hp',
+        value: 90
+      },
+      {
+        identifier: 'mp',
+        value: 80
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/human14.png'
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'unmannedFlyingMachine',
+    schema_identifier: 'wrt_game_character',
+    game_id: '',
+    attributes: [
+      {
+        identifier: 'name',
+        value: '翼型無人兵器'
+      },
+      {
+        identifier: 'hp',
+        value: 130
+      },
+      {
+        identifier: 'mp',
+        value: 50
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/m03.png'
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'heavyDragon',
+    schema_identifier: 'wrt_game_character',
+    game_id: '',
+    attributes: [
+      {
+        identifier: 'name',
+        value: 'ヘビードラゴン'
+      },
+      {
+        identifier: 'hp',
+        value: 200
+      },
+      {
+        identifier: 'mp',
+        value: 110
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/monster04.png'
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'cerberus',
+    schema_identifier: 'wrt_game_character',
+    game_id: '',
+    attributes: [
+      {
+        identifier: 'name',
+        value: 'ケルベロス'
+      },
+      {
+        identifier: 'hp',
+        value: 120
+      },
+      {
+        identifier: 'mp',
+        value: 50
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/monster24.png'
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'manEaterMushroom',
+    schema_identifier: 'wrt_game_character',
+    game_id: '',
+    attributes: [
+      {
+        identifier: 'name',
+        value: '人食いキノコ'
+      },
+      {
+        identifier: 'hp',
+        value: 130
+      },
+      {
+        identifier: 'mp',
+        value: 30
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/plant01.png'
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'movingVine',
+    schema_identifier: 'wrt_game_character',
+    game_id: '',
+    attributes: [
+      {
+        identifier: 'name',
+        value: '動くつた'
+      },
+      {
+        identifier: 'hp',
+        value: 120
+      },
+      {
+        identifier: 'mp',
+        value: 40
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/plant06.png'
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'mimic',
+    schema_identifier: 'wrt_game_character',
+    game_id: '',
+    attributes: [
+      {
+        identifier: 'name',
+        value: 'ミミック'
+      },
+      {
+        identifier: 'hp',
+        value: 100
+      },
+      {
+        identifier: 'mp',
+        value: 90
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/solid02.png'
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'mimic',
+    schema_identifier: 'wrt_game_character',
+    game_id: '',
+    attributes: [
+      {
+        identifier: 'name',
+        value: 'ミミック'
+      },
+      {
+        identifier: 'hp',
+        value: 100
+      },
+      {
+        identifier: 'mp',
+        value: 90
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/solid02.png'
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'bigWaterCreature',
+    schema_identifier: 'wrt_game_character',
+    game_id: '',
+    attributes: [
+      {
+        identifier: 'name',
+        value: '巨大水生生物'
+      },
+      {
+        identifier: 'hp',
+        value: 180
+      },
+      {
+        identifier: 'mp',
+        value: 60
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/aquatic17.png'
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'evilPriest',
+    schema_identifier: 'wrt_game_character',
+    game_id: '',
+    attributes: [
+      {
+        identifier: 'name',
+        value: 'エビルプリースト'
+      },
+      {
+        identifier: 'hp',
+        value: 90
+      },
+      {
+        identifier: 'mp',
+        value: 220
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/human38.png'
+      }
+    ]
+  });
+
+  Objects.insert({
+    identifier: 'deadAngel',
+    schema_identifier: 'wrt_game_character',
+    game_id: '',
+    attributes: [
+      {
+        identifier: 'name',
+        value: 'デッドエンジェル'
+      },
+      {
+        identifier: 'hp',
+        value: 130
+      },
+      {
+        identifier: 'mp',
+        value: 180
+      },
+      {
+        identifier: 'situation',
+        value: 'enemy'
+      },
+      {
+        identifier: 'battleEnemyImageUrl',
+        value: 'https://www.emastation.com/wrt/material/enemyImage/lud.sakura.ne.jp/battler00/undead22.png'
+      }
+    ]
+  });
 }
 
 wrt_fixtureLoadedCount = (typeof wrt_fixtureLoadedCount !== "undefined") ? wrt_fixtureLoadedCount++ : 0;
